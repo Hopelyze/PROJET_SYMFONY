7f764df9b6db2a878d6a1f607f3d44912769deda
Ronan <ronan.pluta@gmail.com>
2025-03-27 09:19:10 +0100

    ajout de la table user et de ses entités. Attention il manque les pays (en attente de la création de la table pays)

--- migrations/Version20250327081728.php
+++ migrations/Version20250327081728.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250326133406 extends AbstractMigration
+final class Version20250327081728 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,15 @@ final class Version20250326133406 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE fleurs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, libelle VARCHAR(200) NOT NULL, prix DOUBLE PRECISION NOT NULL, quantite_stock INTEGER DEFAULT NULL)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, login VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, forename VARCHAR(255) NOT NULL, birthday DATE NOT NULL --(DC2Type:date_immutable)
+        , admin BOOLEAN NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_LOGIN ON user (login)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE fleurs');
+        $this->addSql('DROP TABLE user');
     }
 }
