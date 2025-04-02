0fa74aa4af8398d8a2fcf0e0d86c0ef251e5730a
Ronan <ronan.pluta@gmail.com>
2025-04-02 23:12:27 +0200

    test accessuser et gestion rôle

--- src/Controller/Sandbox/SecurityTestController.php
+++ src/Controller/Sandbox/SecurityTestController.php
@@ -9,6 +9,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/sandbox/securitytest', name: 'sandbox_securitytest')]
 class SecurityTestController extends AbstractController
@@ -18,37 +20,49 @@ class SecurityTestController extends AbstractController
     {
         $user = new User();
         $user   
-            ->setLogin('Riri2')
-            ->setName('Duck')
-            ->setForename('Riri')
-            ->setBirthday(new \DateTimeImmutable('1937-09-22'))
-            ->setAdmin(false)
-            ->setRoles(['ROLE_CUSTOMER']);
-        $hashedPassword = $passwordHasher->hashPassword($user, 'mdpTest');
-        $user->setPassword($hashedPassword);
-        $em->persist($user);
-
-        $user = new User();
-        $user   
-            ->setLogin('Fifi2')
-            ->setName('Duck')
-            ->setForename('Fifi')
-            ->setBirthday(new \DateTimeImmutable('1937-09-22'))
-            ->setAdmin(true)
-            ->setRoles(['ROLE_ADMIN']);
-        $hashedPassword = $passwordHasher->hashPassword($user, 'mdpTest');
-        $user->setPassword($hashedPassword);
-        $em->persist($user);
-
-        $user = new User();
-        $user   
-            ->setLogin('Loulou2')
-            ->setName('Duck')
-            ->setForename('Loulou')
+            ->setLogin('sadmin')
+            ->setName('super')
+            ->setForename('admin')
             ->setBirthday(new \DateTimeImmutable('1937-09-22'))
             ->setAdmin(true)
             ->setRoles(['ROLE_SUPERADMIN']);
-        $hashedPassword = $passwordHasher->hashPassword($user, 'mdpTest');
+        $hashedPassword = $passwordHasher->hashPassword($user, 'nimdas');
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+
+        $user = new User();
+        $user   
+            ->setLogin('gilles')
+            ->setName('Subrenat')
+            ->setForename('Gilles')
+            ->setBirthday(new \DateTimeImmutable('1970-01-01'))
+            ->setAdmin(true)
+            ->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $passwordHasher->hashPassword($user, 'sellig');
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+
+        $user = new User();
+        $user   
+            ->setLogin('rita')
+            ->setName('Zrour')
+            ->setForename('Rita')
+            ->setBirthday(new \DateTimeImmutable('1970-01-01'))
+            ->setAdmin(true)
+            ->setRoles(['ROLE_CUSTOMER']);
+        $hashedPassword = $passwordHasher->hashPassword($user, 'atir');
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+
+        $user = new User();
+        $user   
+            ->setLogin('boumediene')
+            ->setName('Saidi')
+            ->setForename('Boumediene')
+            ->setBirthday(new \DateTimeImmutable('1970-01-01'))
+            ->setAdmin(true)
+            ->setRoles(['ROLE_CUSTOMER']);
+        $hashedPassword = $passwordHasher->hashPassword($user, 'eneidemuob');
         $user->setPassword($hashedPassword);
         $em->persist($user);
 
@@ -56,4 +70,20 @@ class SecurityTestController extends AbstractController
 
         return new Response('<body>AddUsers</body>');
     }
+
+    #[Route('/accessuser', name: '_accessuser')]
+    public function accessUserAction(Security $security): Response
+    {
+        dump($security->getUser());
+        dump($security->getUser()->getRoles());
+        return $this->render('Sandbox/SecurityTest/access_user.html.twig');
+    }
+
+    #[Route('/role1', name: '_role1')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function accessUserAction(): Response
+    {
+        return new Response('<body>IsGranted(\'ROLE_ADMIN\')</body>');
+    }
+
 }
